819955e66b9988eecb8c31661e1ffabca04274c0
Alexandre Reveilleau dos Reis <alexandrerreis@gmail.com>
2020-07-10 21:57:38 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapptictac/screen/game.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Teste",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Game(),
+    );
+  }
+}
